be4b4fd070420c6b35b93fe4378abb7eb95246bc
Ren Delaney <ren.delaney@gmail.com>
2021-03-22 00:31:59 +0000

    Added Tailwind CSS

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.web')
+
+@section('content')
+    <div id="home-body" class="wrap wrap-mountain wrap-keyboard wrap-grey">
+      <div class="container">
+
+        <h1 class="text-center"><strong>Ren Delaney</strong></h1>
+        <h3 class="text-center">Software Engineer & Web Developer</h3>
+        <hr>
+
+        <div class="row">
+          <div class="col-lg-4 order-lg-2 mb-4  center-block">
+            <img id="home-body-img" src="/web/assets/images/me_square.jpg" alt="" class="img-fluid center-block wow zoomIn animation-delay-4 ">
+          </div>
+          <div id="home-txt" class="col-lg-6 order-lg-1 pr-6">
+            <p class="wow fadeInLeft animation-delay-1">
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+
+        <div id="status">
+            <div class="spinner">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+            </div>
+        </div>
+
+        <div class="ms-site-container">
+            @yield('content')
+        </div> <!-- ms-site-container -->
+
+        <script src="/web/assets/js/jquery.min.js"></script>
+        <script src="/web/assets/js/plugins.min.js"></script>
+        <script src="/web/assets/js/app.min.js"></script>
+        <script src="/web/assets/js/ecommerce.js"></script>
+        <script src="/web/assets/js/configurator.min.js"></script>
+    </body>
+</html>
